edab308e3abf137b5968c87ec565b2c68e93f10f
qulub007 <mudaw.qulub@gmail.com>
2026-08-14 14:54:12 +0800

    setup project

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import AppSidebar from './AppSidebar.vue';
+</script>
+<template>
+    <div class="min-h-screen bg-surface-50 flex">
+        <!-- Sidebar -->
+        <AppSidebar />
+
+        <div class="flex-1 ml-64 transition-all duration-300">
+            <div class="p-8 w-full">
+                <RouterView />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/layout/AppSidebar.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const menuItems = ref([
+    {
+        label: "General",
+        items: [
+            { icon: "pi pi-th-large", to: "/", label: "Dashboard" }
+        ]
+    }
+])
+</script>
+<template>
+    <div
+        class="fixed left-0 top-0 h-full w-64 bg-white border-r border-surface-200 flex flex-col z-50 transition-all duration-300">
+
+        <!-- logo -->
+        <div class="h-20 flex items-center px-8 border-b border-surface-100">
+            <div class="w-8 h-8 bg-primary-600 rounded-lg flex items-center justify-center text-white mr-3">
+                <i class="pi pi-receipt text-lg"></i>
+            </div>
+            <span class="text-xl font-bold text-surface-900">MudaPOS</span>
+        </div>
+
+        <!-- menu -->
+        <div class="flex-1 overflow-y-auto py-6 px-4 flex-col gap-6">
+            <div v-for="(section, i) in menuItems" :key="i">
+                <div class="text-xs font-semibold text-surface-400 uppercase tracking-wider mb-3 px-3">
+                    {{ section.label }}
+                </div>
+                <div class="flex flex-col gap-1">
+                    <router-link v-for="(item, j) in section.items" :to="item.to" :key="index"
+                        class="flex items-center gap-3 px-3 py-2.5 rounded-lg transition-colors duration-200"
+                        :class="[route.path == item.to ? 'bg-surface-100 text-primary-600' : 'text-surface-900 hover:bg-surface-100']">
+                        <i :class="[item.icon, 'text-lg']"></i>
+                        <span class="font-medium text-sm">{{ item.label }}</span>
+                    </router-link>
+                </div>
+            </div>
+        </div>
+
+        <!-- User Profile -->
+        <div class="p-4 border-t border-surface-200">
+            <div class="w-full flex items-center gap-3 p-3 rounded-xl hover:bg-surface-50">
+                <div class="w-9 h-9 rounded-full bg-surface-200 flex items-center justify-center overflow-hidden">
+                    <i class="pi pi-user text-lg text-surface-600"></i>
+                </div>
+                <div class="text-left min-w-0 flex-1">
+                    <div class="text-sm font-semibold text-surface-900 truncate">
+                        Mudawil Qulub
+                    </div>
+                    <div class="text-xs text-surface-500 truncate">
+                        mudawil@mudapos.com
+                    </div>
+                </div>
+                <div
+                    class="ml-auto w-8 h-8 rounded-lg flex items-center justify-center text-surface-400 group-hover:bg-red-100 group-hover:text-red-600 transition-colors flex-shrink-0">
+                    <i class="pi pi-sign-out text-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
